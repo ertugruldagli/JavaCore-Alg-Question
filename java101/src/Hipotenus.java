75eb4d6b8edc95a1e7f42a272b958ecaf251e41e
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-09-12 16:27:53 +0300

    commitlendi

--- java101/src/Hipotenus.java
+++ java101/src/Hipotenus.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 public class Hipotenus {
     public static void main(String[] args) {
-        double a,b,c;
+        double a,b,t;
 
         Scanner scn = new Scanner(System.in);
         System.out.println("1. kenari gir: ");
         a=scn.nextDouble();
         System.out.println("2. kenari gir: ");
         b=scn.nextDouble();
-        //double t=Math.pow(a,2)+Math.pow(b,2);
-        c=Math.sqrt((a*a)+(b*b));
 
-        System.out.Println("sonuc:"+c);
+        t= Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.println(t);
+
+
+
 
 
 
