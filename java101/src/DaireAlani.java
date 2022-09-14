4e37a1a0ef03253d16604e3da7c7d410694912d4
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-09-14 15:36:51 +0300

    Odev4 commitlendi.

--- java101/src/DaireAlani.java
+++ java101/src/DaireAlani.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class DaireAlani {
+    public static void main(String[] args) {
+
+        int r; double pi=3.14, alan, alfa;
+
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Yaricap Giriniz: ");
+        r= scn.nextInt();
+
+        System.out.println("mao Giriniz: ");
+        alfa= scn.nextInt();
+
+        alan= (pi*(r*r)*alfa)/360;
+
+
+        System.out.println("Alani: " + alan);
+
+
+
+
+    }
 
 }
