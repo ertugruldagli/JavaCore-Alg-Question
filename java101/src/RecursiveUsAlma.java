83469d7b521aabea32a3ce591a18afd93e41fd7c
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-05 14:42:06 +0300

    Recursive Us Almacommitlendi

---
+++ java101/src/RecursiveUsAlma.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RecursiveUsAlma {
+    static int pow(int n,int k){
+        int total=1;
+        for (int i=0;i<k;i++){
+            return total*=n;
+        }
+        return pow(2,3) ;
+    }
+
+
+    public static void main(String[] args) {
+    // yarida kaldi tamamlanacak!!!!
+    }
+}
