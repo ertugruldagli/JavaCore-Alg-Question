965db6d93c9b2577dd175fb906654c4b58e924b6
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-05 15:07:21 +0300

    Teacher class commit

--- java101/src/obs/Teacher.java
+++ java101/src/obs/Teacher.java
@@ -1,4 +1,19 @@
 package obs;
 
 public class Teacher {
+    String mpno;
+    String name;
+    String branch;
+
+    Teacher ( String name,  String branch, String mpno){
+        this.name=name;
+        this.branch=branch;
+        this.mpno=mpno;
+    }
+
+    void print(){
+        System.out.println( "Teacher: "+this.name+ "\nDal: "+ this.branch + "\nPhone: "+this.mpno);
+    }
+
+
 }
