53dd6427d3af0a801fa08d54e29815f498299bcd
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-05 15:08:13 +0300

    Courses class commit

--- java101/src/obs/Courses.java
+++ java101/src/obs/Courses.java
@@ -1,4 +1,31 @@
 package obs;
 
 public class Courses {
+
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+
+    Courses(String name, String code, String prefix){
+        this.name=name;
+        this.code=code;
+        this.prefix=prefix;
+        int note=0; //this.note=0;
+    }
+    void addTeacher(Teacher teacher){
+
+        if(teacher.branch.equals(this.prefix)){
+            this.teacher=teacher;
+            //printTeacher();
+        }else{
+            System.out.println("Yanlis kod girdiniz.");
+        }
+
+    }
+
+    void printTeacher(){
+        this.teacher.print();
+    }
 }
