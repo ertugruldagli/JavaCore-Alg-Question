36ef7ffaf2af93af604a6b68ce43c34a219e21b1
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-05 15:00:35 +0300

    harmonik commmit

--- java101/src/Harmonik.java
+++ java101/src/Harmonik.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Harmonik {
     public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Bir sayi giriniz: ");
+        int n=inp.nextInt();
+        double total=0;
+
+        //with for loop
+        for(double i=1; i<=n; i++){
+            total+= (1/i);
+        }
+        System.out.println(" for -- Harmonik Seri Toplami: " + total);
+
+
+        //with while loop
+        double sayac=1;
+        while(sayac<=n){
+            total+= (1/sayac);
+            sayac++;
+        }
+        System.out.println(" While -- Harmonik Seri Toplami: " + total);
 
     }
 }
