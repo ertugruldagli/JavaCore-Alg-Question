36943074391754fdaa88899b7319ab7217bb03c8
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-10 12:21:00 +0300

    commit

--- java101/src/Login.java
+++ java101/src/Login.java
@@ -5,14 +5,25 @@ public class Login {
 
         String sifre=new String("1234");
 
+
         Scanner input=new Scanner (System.in);
         System.out.println("Sifreyi girininz: ");
         String pass= input.nextLine();
 
         if(pass.equals(sifre)){
             System.out.println("Sifre dogru giris yaptiniz.");
+
         }else{
-            System.out.println("Yanlis girdiniz daha sonra tekrar deneyiniz!");
+            System.out.println("Hatali Sifre!!!  daha sonra tekrar deneyiniz!");
+            System.out.println("Sifreyi sifirlamak ister misiniz? e/h");
+
+            String e= input.nextLine();
+            if (e=="e"){
+                System.out.println();
+            }else{
+                System.out.println("sifirlama islemi basarisiz..");
+            }
+
         }
 
     }
