70d05e792b1a698594558e0c680ce6afcf1c1a84
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-10 12:12:30 +0300

    commit

--- java101/src/Login.java
+++ java101/src/Login.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Login {
     public static void main(String[] args) {
+
+        String sifre=new String("1234");
+
+        Scanner input=new Scanner (System.in);
+        System.out.println("Sifreyi girininz: ");
+        String pass= input.nextLine();
+
+        if(pass.equals(sifre)){
+            System.out.println("Sifre dogru giris yaptiniz.");
+        }else{
+            System.out.println("Yanlis girdiniz daha sonra tekrar deneyiniz!");
+        }
 
     }
 }
