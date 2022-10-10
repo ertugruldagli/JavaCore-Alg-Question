1e7afb196ad642bfe35614aefc5ca6a68cae28de
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-10 12:34:49 +0300

    son commit

--- java101/src/Login.java
+++ java101/src/Login.java
@@ -4,11 +4,12 @@ public class Login {
     public static void main(String[] args) {
 
         String sifre=new String("1234");
+        String pass,e;
 
 
         Scanner input=new Scanner (System.in);
         System.out.println("Sifreyi girininz: ");
-        String pass= input.nextLine();
+        pass= input.nextLine();
 
         if(pass.equals(sifre)){
             System.out.println("Sifre dogru giris yaptiniz.");
@@ -16,12 +17,12 @@ public class Login {
         }else{
             System.out.println("Hatali Sifre!!!");
             System.out.println("Sifreyi sifirlamak ister misiniz? e/h");
-            String e= input.nextLine();
+            e= input.nextLine();
 
             if (e.equals("e")){
                 System.out.println("Yeni Sifreyi giriniz: ");
-                String pas= input.nextLine();
-                if (pas.equals(sifre)){
+                 pass= input.nextLine();
+                if (pass.equals(sifre)){
                     System.out.println("Yeni Sifre Eski Sifre ile ayni olamaz!! ");
                 }else{
                     System.out.println("Yeni sifre olusturuldu.");
