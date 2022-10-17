e110fafb761fbed88c33cf28dd73b3c4efcf7eac
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-17 15:07:53 +0300

    secim yapildi commit

--- java101/src/FlyTicket.java
+++ java101/src/FlyTicket.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class FlyTicket {
     public static void main(String[] args) {
         int line, old;
-        String type;
-        double yasSell, normalPrice,sell;
+        int selected;
+        double yasSell, normalPrice;
 
         Scanner inp=new Scanner(System.in);
         System.out.print("Mesafeyi giriniz: ");
@@ -13,26 +13,34 @@ public class FlyTicket {
         old=inp.nextInt();
 
         normalPrice=line*0.10;
-        System.out.println("Mesafeli fiyat: " + normalPrice + " TL ");
 
-        if(old<=12){
-            yasSell=normalPrice*0.50;
-            System.out.println("Yas indirimi fiyat: "+yasSell + " TL ");
-            sell=normalPrice-yasSell;
-            System.out.println(" indirimli fiyat: "+sell + " TL ");
-        } else if (old>12 && old<=24) {
-            yasSell=normalPrice*0.10;
-            System.out.println("Yas indirimi fiyat: "+yasSell + " TL ");
-            sell=normalPrice-yasSell;
-            System.out.println(" indirimli fiyat: "+sell + " TL ");
-        } else if (old>65) {
-            yasSell=normalPrice*0.30;
-            System.out.println("Yas indirimi fiyat: "+yasSell + " TL ");
-            sell=normalPrice-yasSell;
-            System.out.println(" indirimli fiyat: "+sell + " TL ");
-        }else{
-            System.out.println("fiyat: "+normalPrice + " TL ");
+        System.out.println("Tek Yon=> 1 , Cift Yon=> 2     Secim Yapiniz!!!!");
+        selected=inp.nextInt();
+
+      if(selected==2){
+            if(old<=12){
+                yasSell=normalPrice*0.50;
+                normalPrice-=yasSell;
+
+            } else if (old>12 && old<=24) {
+                yasSell=normalPrice*0.10;
+                normalPrice-=yasSell;
+
+
+            } else if (old>65) {
+                yasSell=normalPrice*0.30;
+                normalPrice-=yasSell;
+
+            }else{
+                System.out.println("fiyat: "+normalPrice + " TL ");
+            }
+
+            double gd= normalPrice*0.20;
+            double total= (normalPrice-gd)*2;
+            System.out.println("Toplam tutar: " + total + " Tl ");
+
         }
+
 
     }
 
