4506d3427ea150b086c9a45b58a89bce7b8fcd44
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-17 15:25:19 +0300

    son commit

--- java101/src/GirilenSayiKuvvetAlma.java
+++ java101/src/GirilenSayiKuvvetAlma.java
@@ -8,13 +8,12 @@ public class GirilenSayiKuvvetAlma {
         System.out.print("Bir sayi giriniz:");
         n=input.nextInt();
 
-        while(sayac<=n ){
+        while(sayac<=n  || i<=n){
             System.out.println("dort ussu: "+ sayac);
             sayac*=4;
-            if(i<=n){
-                System.out.println("bes ussu:---------- "+ i);
-                i*=5;
+            System.out.println("bes ussu:---------- "+ i);
+            i*=5;
             }
-        }
+
     }
 }
