4a30363a7e1c377e9e8339ed7c7aeb128b117a5d
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-17 14:21:21 +0300

    indirimli tutar commit

--- java101/src/FlyTicket.java
+++ java101/src/FlyTicket.java
@@ -4,7 +4,7 @@ public class FlyTicket {
     public static void main(String[] args) {
         int line, old;
         String type;
-        double yasSell, normalPrice;
+        double yasSell, normalPrice,sell;
 
         Scanner inp=new Scanner(System.in);
         System.out.print("Mesafeyi giriniz: ");
@@ -18,12 +18,18 @@ public class FlyTicket {
         if(old<=12){
             yasSell=normalPrice*0.50;
             System.out.println("Yas indirimi fiyat: "+yasSell + " TL ");
+            sell=normalPrice-yasSell;
+            System.out.println(" indirimli fiyat: "+sell + " TL ");
         } else if (old>12 && old<=24) {
             yasSell=normalPrice*0.10;
             System.out.println("Yas indirimi fiyat: "+yasSell + " TL ");
+            sell=normalPrice-yasSell;
+            System.out.println(" indirimli fiyat: "+sell + " TL ");
         } else if (old>65) {
             yasSell=normalPrice*0.30;
             System.out.println("Yas indirimi fiyat: "+yasSell + " TL ");
+            sell=normalPrice-yasSell;
+            System.out.println(" indirimli fiyat: "+sell + " TL ");
         }else{
             System.out.println("fiyat: "+normalPrice + " TL ");
         }
