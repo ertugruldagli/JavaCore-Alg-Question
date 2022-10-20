699b3563caa1bbb9b35c11e577684c9bf3dce013
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-20 13:42:07 +0300

    commit

--- java101/src/Maas/Main.java
+++ java101/src/Maas/Main.java
@@ -5,7 +5,7 @@ package Maas;
 public class Main {
     public static void main(String[] args) {
         Employee e1=new Employee("Ertugrul DAGLI", 2000, 45, 1985);
-        e1.tostring();
+        e1.tostring(); //
     }
 
 
